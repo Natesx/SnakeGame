6f0df4c973f694c53460deab512e1e3ab2eda677
Natesx <kokobins656@gmail.com>
2024-06-09 19:05:27 +0200

    jeszcze raz

--- SnakeGame/obj/Release/net8.0/SnakeGame.AssemblyInfo.cs
+++ SnakeGame/obj/Release/net8.0/SnakeGame.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakeGame")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8382bf88239e9a8623a226e7974ee0f4ca2fb4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakeGame")]
