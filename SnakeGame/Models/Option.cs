c6c519c657d7b1598491b7f272e6f6ac8d09599d
Natesx <kokobins656@gmail.com>
2024-06-09 18:54:23 +0200

    halo

---
+++ SnakeGame/Models/Option.cs
@@ -0,0 +1,19 @@
+﻿namespace SnakeGame.Models
+{
+    public class Option
+    {
+        public string Name { get; }
+        public Action Selected { get; }
+
+        public Option(string name, Action selected)
+        {
+            Name = name;
+            Selected = selected;
+        }
+
+        public static implicit operator Option(List<Option> options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SnakeGame/obj/Debug/net8.0/SnakeGame.AssemblyInfo.cs
+++ SnakeGame/obj/Debug/net8.0/SnakeGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakeGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520406eaf508756d4096cfe8469bab664e25b392")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakeGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakeGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
